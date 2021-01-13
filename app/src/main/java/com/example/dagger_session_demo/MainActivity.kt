c295767024f9797142943fc70450be02c0ecf605
Hassan Khamis <52527199+hassankhamis97@users.noreply.github.com>
2021-01-13 15:55:11 +0200

    use Create()

--- app/src/main/java/com/example/dagger_session_demo/MainActivity.kt
+++ app/src/main/java/com/example/dagger_session_demo/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val product = DaggerAppComponent.builder().build().product
+        val product = DaggerAppComponent.create().product
         Log.d(TAG, "onCreate: $product")
     }
 }
